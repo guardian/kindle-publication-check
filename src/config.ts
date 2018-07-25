3f307cc43bccc2590e25d7f3fd0a5e11c406f678
tomrf1 <tom.forbes@theguardian.com>
2018-07-25 14:35:47 +0100

    trim

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export class Config {
     KindleBucket: string = process.env.KindleBucket;
     Stage: string = process.env.Stage;
     SourceAddress: string = process.env.SourceAddress;
-    PassTargetAddresses: string[] = process.env.PassTargetAddresses.split(',');
-    FailureTargetAddresses: string[] = process.env.FailureTargetAddresses.split(',');
+    PassTargetAddresses: string[] = process.env.PassTargetAddresses.split(',').map(a => a.trim());
+    FailureTargetAddresses: string[] = process.env.FailureTargetAddresses.split(',').map(a => a.trim());
     MinimumArticleCount: number = parseInt(process.env.MinimumArticleCount);
 
     Today: string = moment().format('YYYY-MM-DD');
