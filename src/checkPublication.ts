31d7648cf74e6beb95eed83bbd75579a819636d4
Regis Kuckaertz <regis.kuckaertz@guardian.co.uk>
2019-05-20 12:02:48 +0100

    Report on failures in logs

--- src/checkPublication.ts
+++ src/checkPublication.ts
@@ -2,7 +2,7 @@ import { ListObjectsOutput } from "aws-sdk/clients/s3";
 import { SendEmailRequest, SendEmailResponse } from "aws-sdk/clients/ses";
 import { Config } from "./config";
 import { URL } from "url";
-import AWS from "aws-sdk";
+import * as AWS from "aws-sdk";
 import { request, RequestOptions } from "http";
 
 let s3 = new AWS.S3();
@@ -15,6 +15,8 @@ type PublicationInfo = {
 };
 
 export function checkPublication(config: Config): Promise<SendEmailResponse> {
+    const logGroupName: string = `/aws/lambda/kindle-gen-${config.Stage}`;
+
     //This lambda runs either after midnight or after 1am. Default to 1am in case we want to manually run later
     let currentHourString = () => (new Date().getHours() === 0 ? "0000" : "0100");
 
@@ -101,6 +103,52 @@ export function checkPublication(config: Config): Promise<SendEmailResponse> {
         }
     };
 
+    const getLogs = (logGroupName: string): Promise<Array<AWS.CloudWatchLogs.OutputLogEvent>> => findLogStreams(logGroupName).then(getCloudwatchLogs(logGroupName));
+
+    const findLogStreams = (logGroupName: string): Promise<Array<AWS.CloudWatchLogs.LogStream>> => new Promise((resolve, reject) => {
+        logs.describeLogStreams({
+            logGroupName,
+            orderBy: 'LastEventTime',
+            descending: true,
+            limit: 2
+        }, function (err, data) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(data.logStreams);
+        })
+    });
+
+    const getCloudwatchLogs = (logGroupName: string) => (logStreams: AWS.CloudWatchLogs.LogStream[]): Promise<Array<AWS.CloudWatchLogs.OutputLogEvent>> => {
+        const getLogsFor = (logStream: AWS.CloudWatchLogs.LogStream): Promise<AWS.CloudWatchLogs.OutputLogEvent[]> => new Promise((resolve, reject) => {
+            logs.getLogEvents({
+                logGroupName,
+                logStreamName: logStream.logStreamName,
+                startFromHead: true
+            }, (err, data) => {
+                if (err) {
+                    reject(err);
+                    return;
+                } else if (data.events.length < 20) {
+                    throw new Error(`Log stream ${logStream.logStreamName} in ${logGroupName} only contains ${data.events.length} but we expected hundreds of them`)
+                }
+
+                resolve(data.events);
+            })
+        });
+        return getLogsFor(logStreams[0]).catch(() => getLogsFor(logStreams[1]));
+    }
+
+    const checkLogs = (allLogs: Array<AWS.CloudWatchLogs.OutputLogEvent>): Promise<void> => {
+        const errors = allLogs.filter(log => / WARN | ERROR | FATAL /.test(log.message))
+        if (errors.length > 0) {
+            return Promise.reject(errors)
+        } else {
+            return Promise.resolve();
+        }
+    }
+
     let sendEmail = (
         subject: string,
         body: string,
@@ -151,6 +199,7 @@ export function checkPublication(config: Config): Promise<SendEmailResponse> {
 
     return getRedirect(config.ManifestURL)
         .then(testRedirect)
+        .then(() => getLogs(logGroupName).then(checkLogs))
         .then(() =>
             getS3Objects(
                 config.KindleBucket,
