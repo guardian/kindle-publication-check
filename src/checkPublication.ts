9f777d49cf375a2f1d27e3f2f5a698fc62c5e8da
Fweddi <freddie.preece@guardian.co.uk>
2021-07-02 14:58:20 +0100

    Return error message instead of error object

--- src/checkPublication.ts
+++ src/checkPublication.ts
@@ -176,7 +176,7 @@ export function checkPublication(
     allLogs: Array<AWS.CloudWatchLogs.OutputLogEvent>
   ): Promise<void> => {
     const errorRegexp = /WARN|ERROR|FATAL/;
-    const errors = allLogs.filter(log => errorRegexp.test(log.message));
+    const errors = allLogs.filter(log => errorRegexp.test(log.message)).map(log => log.message);
     if (errors.length > 0) {
       return Promise.reject(errors);
     } else {
